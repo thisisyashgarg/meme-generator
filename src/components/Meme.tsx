f05cddd00a51a0ca91cd2c6769e564eecef77ba3
Yash Garg <thisisyashgarg@gmail.com>
2023-01-17 19:28:46 +0530

    fetched data and displayed random meme templates, use of use State

--- src/components/Meme.tsx
+++ src/components/Meme.tsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+  const [imageURL, setImageURL] = useState("");
+
+  function logURL() {
+    const memes = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memes.length);
+    return memes[randomNumber].url;
+  }
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
-        <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+        <button className="form--button" onClick={() => setImageURL(logURL())}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img src={imageURL} />
     </main>
   );
 }
