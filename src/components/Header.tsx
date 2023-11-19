1d69efc8e81ab3b1011e532fa1c8f705889d00df
Yash Garg <thisisyashgarg@gmail.com>
2023-11-19 21:59:49 +0530

    fix: footer removed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,9 @@ export default function Header() {
     <header className="header">
       <img src={logo} className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
-      <h4 className="header--project">
+      {/* <h4 className="header--project">
         Created by <a href="https://linktr.ee/thisisyashgarg">Yash Garg</a>
-      </h4>
+      </h4> */}
     </header>
   );
 }
